aaca45654b949611690fc11d70d8283cab656ba1
jiglesia22 <jiglesia@minsait.com>
2024-12-16 11:57:22 +0000

    fix: set tests directory, download python extension and modify SparkSession in conftest so it uses Iceberg Catalog

--- bootcamp/materials/3-spark-fundamentals/src/tests/conftest.py
+++ bootcamp/materials/3-spark-fundamentals/src/tests/conftest.py
@@ -3,7 +3,10 @@ from pyspark.sql import SparkSession
 
 @pytest.fixture(scope='session')
 def spark():
-    return SparkSession.builder \
-      .master("local") \
-      .appName("chispa") \
-      .getOrCreate()
+  return SparkSession.builder \
+    .appName("chispa") \
+    .master("local") \
+    .config("spark.sql.catalog.demo", "org.apache.iceberg.spark.SparkCatalog") \
+    .config("spark.sql.catalog.demo.type", "rest") \
+    .config("spark.sql.catalog.demo.uri", "http://localhost:8181") \
+    .getOrCreate()
